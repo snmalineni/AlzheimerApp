3c9eba6da12077f6903ea7def8ba0eda5e4b463b
Sneha Malineni <sneha@Snehas-MacBook-Air.local>
2016-07-28 19:55:06 -0700

    Added clear functionality to upload photo button

---
+++ app/src/main/java/Source/nextPhoto.java
@@ -0,0 +1,28 @@
+package Source;
+
+import android.view.View;
+import android.media.Image;
+import android.sax.TextElementListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.net.Uri;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.widget.TextView;
+import android.widget.Button;
+import Source.cappedPhoto;
+import android.content.SharedPreferences;
+import android.view.View.OnClickListener;
+
+/**
+ * Created by sneha on 7/28/16.
+ */
+public class nextPhoto  {
+
+
+
+}
--- app/src/main/java/com/sneha/alzapp9/MainActivity.java
+++ app/src/main/java/com/sneha/alzapp9/MainActivity.java
@@ -1,49 +1,107 @@
 package com.sneha.alzapp9;
 
+import android.media.Image;
+import android.sax.TextElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.net.Uri;
 import android.graphics.BitmapFactory;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.widget.TextView;
+import android.widget.Button;
+import Source.cappedPhoto;
+import android.content.SharedPreferences;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity{
+
+    public boolean newPhoto = true;
     private int PICK_IMAGE_REQUEST = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        cappedPhoto photoAr[] = new cappedPhoto[150];
+        int arIndex = 0;
+
+        // String photoAr2[] = new String[150];
+        //int arIndex = 0;
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+
+
+
+        }
+
+
+
+    public void buttonClick(View v) {
+        EditText nameText = (EditText) findViewById(R.id.nameText);
+        //Finds current status of text field when done button clicked
+        String inputText = nameText.getText().toString();
+        //Takes input from text field and puts it into variable inputText
+        String photoStr = uri.toString();
+        photoAr[arIndex] = new cappedPhoto(photoStr, inputText);
+        //Creates an instance of cappedPhoto in the array
+        System.out.println(photoAr[arIndex].getCap());
+        System.out.println(photoAr[arIndex].getUriString());
+        //DELETE BEFORE BIG PRESENTATION!!!!!!!
+        arIndex++;
+
     }
 
-    public void onButtonClicked( View arg0)
-    {
+    public void onButtonClicked(View arg0) {
+        EditText nameText = (EditText)findViewById(R.id.nameText);
+        nameText.setText("", TextView.BufferType.EDITABLE);
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageBitmap(null);
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_REQUEST);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+        //When upload photos button clicked goes into gallery and allows user to select
     }
+
+    Uri uri;
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
 
-        Uri uri = data.getData();
-        try {
-            Bitmap thumbnail = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-            //Log.d(TAG, String.valueOf(bitmap));
+            uri = data.getData();
 
-            ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            imageView.setImageBitmap(thumbnail);
-        }   catch (Exception e) {
-            e.printStackTrace();
-        }
+
+            try {
+                Bitmap thumbnail = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                //Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(thumbnail);
+                //Sets photo into imageView on screen
+            } catch (Exception e) {
+                e.printStackTrace();
+                //Happens if photo is null
+            }
+
 
         }
     }
+
+
+
 }
+
+
+
